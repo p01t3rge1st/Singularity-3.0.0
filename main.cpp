2907c57c75f87cf531395123775897fd535669cd
p01t3rge1st <kaminskimateusz.p01@gmail.com>
2025-03-11 19:43:41 +0100

    Initial ideas

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
 #include "airnelectro.h"
 #include "psu.h"
 #include "gui.h"
+#include <string>
+#include <iostream>
 
 int main() {
+
+    std::cout << '\n';
+
+    std::cout << R"(┏┓•      ┓    •     ┏┓
+┗┓┓┏┓┏┓┓┏┃┏┓┏┓┓╋┓┏   ┫
+┗┛┗┛┗┗┫┗┻┗┗┻┛ ┗┗┗┫  ┗┛
+      ┛          ┛    
+
+)" << '\n';
+
+    std::cout << "Version 3.0.0 Raspberry Pi Main Control Unit" << std::endl;
 
     AirnElectro airnelectro;
     PSU psu;
@@ -11,7 +24,7 @@ int main() {
     airnelectro.start();
     psu.powerOn();
     gui.run();
-    
+
 
     return 0;
 }
